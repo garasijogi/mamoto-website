3aba6c436d6d94227aae62bf0556009939c9ac56
algorithmsEvolve <akbarriski9404@gmail.com>
2021-01-17 23:13:42 +0700

    #1.2 FUCK rizukiRyumada
    create css and js for every developers to avoid any conflict

---
+++ resources/views/layouts/css/al-styles.blade.php
@@ -0,0 +1,4 @@
+{{-- styles --}}
+<link href="{{ asset('css/algorithm/styles.css') }}" rel="stylesheet">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
