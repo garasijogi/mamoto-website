8335dbc7d42445e346192d1c5517777578e72220
Rizuki Ryumada <rizki3366@msn.cn>
2021-02-04 16:19:55 +0700

    updated laravel mix sources

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,15 +16,18 @@ mix.js('resources/js/app.js', 'public/js');
 /* --------------------------- about script mixer --------------------------- */
 mix.scripts([
         'resources/js/admin/about_summernote.js',
+        'resources/js/admin/about_summernote_fileUpload.js',
     ], 'public/js/admin/about.js');
 /* --------------------------- admin script mixer --------------------------- */
 mix.scripts([
         'resources/js/admin/swal_toast.js', // toast using sweet alert2
     ], 'public/js/admin/_admin.js')
+mix.copy('resources/js/admin/jquery-file-upload', 'public/js/admin/jquery-file-upload')
 /* ------------------------ summernote custom script ------------------------ */
 /**
  * This summernote custom script added responsive to video and image
  */
-mix.copy('resources/js/admin/summernote-custom/summernote-bs4.js', 'public/vendor/summernote/summernote-bs4.js')
+mix.copy('resources/js/admin/summernote-custom/summernote-bs4.js', 'public/vendor/summernote/summernote-bs4.js');
+mix.copy('resources/js/admin/summernote-gallery/summernote-gallery.min.js', 'public/js/admin/summernote-gallery/summernote-gallery.min.js');
 
 mix.sass('resources/sass/app.scss', 'public/css');
