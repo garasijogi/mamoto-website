3645e373044ed88e0eae013ab5fdc145eb05f132
Rizuki Ryumada <rizki3366@msn.cn>
2021-02-20 23:28:19 +0700

    edit route to add get promo

--- routes/web.php
+++ routes/web.php
@@ -57,6 +57,7 @@ Route::prefix('admin')->middleware('auth')->group(function () {
 
     //2.6) Kelola Promo
     Route::get('promo', 'KelolaPromoController@index')->name('admin.promo');
+    Route::get('promo/get', 'KelolaPromoController@get')->name('admin.promo.get');
     Route::post('promo/add', 'KelolaPromoController@add')->name('admin.promo.add');
 
     //2.7) Kelola FAQ
