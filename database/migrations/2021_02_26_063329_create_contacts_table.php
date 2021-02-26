e5f26717631109e1ba04aa43c7585300912cd8f7
Rizuki Ryumada <rizki3366@msn.cn>
2021-02-26 15:15:11 +0700

    initial contact module
    + model, controllers, migrations, seeder

---
+++ database/migrations/2021_02_26_063329_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // create contact table
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64)->unique();
+            $table->string('contact', 128);
+            $table->string('logo', 64);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
