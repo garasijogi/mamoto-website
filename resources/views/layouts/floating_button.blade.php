8e350e81832cd368401a31cde88823f34c42b927
ryumada <rizukiryumada@gmail.com>
2022-01-12 10:38:04 +0700

    enhance contact to add link message
    - remove contact text settings

--- resources/views/layouts/floating_button.blade.php
+++ resources/views/layouts/floating_button.blade.php
@@ -1,4 +1,4 @@
-<a class="al-fb" target="_blank" href='<?= "{$whatsapp->link}{$whatsapp->contact}?text={$whatsapp->text}" ?>'>
+<a class="al-fb" target="_blank" href='<?= "{$whatsapp->link}{$whatsapp->contact}" ?>'>
   <h6>Contact Us</h6>
   <img src="/icons/wa_green_icon.png" alt="contact_us">
 </a>
